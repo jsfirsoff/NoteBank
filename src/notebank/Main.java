e2e0e1c7a460cfd591bd0102f626ca0959d29fae
jsfirsoff <26422116+jsfirsoff@users.noreply.github.com>
2021-11-16 18:23:12 -0500

    work on view
    add tags on view side
    add textboxes on view
    add button on view

--- src/notebank/Main.java
+++ src/notebank/Main.java
@@ -15,13 +15,6 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("view/notebank.fxml"));
         primaryStage.setTitle("NoteBank v0.1");
         
-        
-       // Button button1 = new Button();
-        
-      //  VBox vbox = new VBox(button1);
-        
-       // root.getChildrenUnmodifiable().add(vbox);
-        
         Scene scene = new Scene(root, 1000, 600);
         scene.getStylesheets().add(getClass().getResource("view/stylesheet.css").toExternalForm());
         primaryStage.setScene(scene);
--- src/notebank/controller/Controller.java
+++ src/notebank/controller/Controller.java
@@ -1,22 +1,71 @@
 package notebank.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
 public class Controller {
 
     @FXML
-    private Label label;
+    private TextArea title;
+    
+    @FXML
+    private Button toggleBtn;
+    
+    @FXML
+    private TextArea text;
+    
+    @FXML
+    private TextField tags;
+    
+    @FXML
+    private FlowPane tagBox;
 
     public void initialize() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+        title.setText("Title here");
+        text.setText("note here");
     }
      
     @FXML
     private void printOutput() 
     {
         System.out.println("Sup");
+    }
+    
+    @FXML
+    private void toggleSearch() 
+    {
+        System.out.println("Toggle Search");
+        String btntext = toggleBtn.getText();
+        
+        if (btntext.equals(">")) {
+        	toggleBtn.setText("<");
+        }
+        else {
+        	toggleBtn.setText(">");
+        }
+    }
+    
+    @FXML
+    private void onEnter(ActionEvent event) {
+    	String text = tags.getText();
+    	System.out.println("new tag: " + text);
+    	createTag(text);
+    	tags.clear();
+    }
+    
+    private void createTag(String text) {
+    	//todo: create tag
+    	Label label = new Label(text);
+    	label.getStyleClass().add("tag");
+    	
+    	int len = tagBox.getChildren().size();
+    	tagBox.getChildren().add(len-1, label);
     }
 }
